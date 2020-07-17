50e323c962d0d564caa466b6365bca72e0389371
Ascendere_Lab <ascendere_lab@iMac-de-Charlie.local>
2020-07-17 12:25:12 -0500

    prepare to deploy

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/public/'));
+
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+let port = process.env.PORT || 8080
+app.listen(port, ()=> console.log('300'));
